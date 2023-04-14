e48ced5908f8d7156b1c58a9c0d64186666d6bcb
mghazidev <mghazidev@gmail.com>
2023-04-14 19:44:18 +0500

    Automobile

--- src/Contact.js
+++ src/Contact.js
@@ -1,24 +1,37 @@
 import React from 'react';
 import './contact.css';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+
 
 function Contact() {
     return (
         <>
             <div className='contact-component'>
                 <div className='contact'>
-                <form className='table' action="send-email.php" method="post">
-                            <label for="name">Name:</label> 
-                            <input type="text" id="name" name="name" placeholder="Name" required />
-                             <label for="email">Email:</label>
-                            <input type="email" id="email" name="email" placeholder="Email" required />
-                             <label for="subject">Subject:</label>
-                            <input type="text" id="subject" name="subject" placeholder="Subject" required />
-                            <label for="message">Message</label>
-                            <textarea id="message" name="message" rows= '6' placeholder='Message' required></textarea>
+                    <div className='info'>
+                        <h2>Need additional information? </h2>
+                        <p>A multifaceted professional skilled in multiple fields of research, development as well as a learning specialist. Over 15 years of experience.</p>
+                        <div className='information'>
+                        <p>(123) 456-7869</p>
+                        <p>carrental@carmail.com</p>
+                        <p>Belgrade, Serbia</p>
+                        </div>
+                    </div>
+                    <div className='info'>
+                        <form className='table'>
+                            <label for="name">Full Name:</label>
+                            <input type="text" id="name" name="name" placeholder="E:g: Example" required />
+                            <label for="email">Email:</label>
+                            <input type="email" id="email" name="email" placeholder="youremail@gmail.com" required />
+                            <label for="message">Tell us about it</label>
+                            <textarea id="message" name="message" rows='6' placeholder='Write here' required></textarea>
                             <button type="submit">Send Message</button>
                         </form>
+                    </div>
                 </div>
             </div>
+            <Footer />
         </>
     )
 }
